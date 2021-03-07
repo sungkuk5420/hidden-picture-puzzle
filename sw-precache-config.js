431094f8274484186607906773a09f23d409c96d
sungkuk5420 <sungkuk5420@gmail.com>
2021-03-07 10:17:36 +0900

    pwa 빌드 설정

---
+++ sw-precache-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    staticFileGlobs: [
+      'app/css/**.css',
+      'app/**.html',
+      'app/images/**.*',
+      'app/js/**.js'
+    ],
+    stripPrefix: 'app/',
+    runtimeCaching: [{
+      urlPattern: /this\\.is\\.a\\.regex/,
+      handler: 'networkFirst'
+    }]
+  };
