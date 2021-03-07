ffe2048a1a6ee339cb87b126e1613ac7f58d4442
sungkuk5420 <sungkuk5420@gmail.com>
2021-03-07 20:08:37 +0900

    pwa설정

--- quasar.conf.js
+++ quasar.conf.js
@@ -118,27 +118,27 @@ module.exports = function (/* ctx */) {
         theme_color: '#027be3',
         icons: [
           {
-            src: 'icons/icon-128x128.png',
+            src: 'icons/icon.png',
             sizes: '128x128',
             type: 'image/png'
           },
           {
-            src: 'icons/icon-192x192.png',
+            src: 'icons/icon.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icons/icon-256x256.png',
+            src: 'icons/icon.png',
             sizes: '256x256',
             type: 'image/png'
           },
           {
-            src: 'icons/icon-384x384.png',
+            src: 'icons/icon.png',
             sizes: '384x384',
             type: 'image/png'
           },
           {
-            src: 'icons/icon-512x512.png',
+            src: 'icons/icon.png',
             sizes: '512x512',
             type: 'image/png'
           }
